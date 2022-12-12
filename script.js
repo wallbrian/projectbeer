a17d3b6af2909fa749db8d582043603fe57aac91
wallbrian <116678290+wallbrian@users.noreply.github.com>
2022-12-12 20:17:14 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,169 @@
-function reveal(){for(var e=document.querySelectorAll(".reveal"),n=0;n<e.length;n++){var o,r=window.innerHeight;e[n].getBoundingClientRect().top<r-150?e[n].classList.add("active"):e[n].classList.remove("active")}}window.addEventListener("scroll",reveal);let shopList=[];function jumperList(){document.getElementById("shopMain").innerHTML=shopList.toString()}function addToEndJumper(){shopList.push("Jumper"),jumperList()}function removeFromEndJumper(){shopList.pop(),jumperList()}let shopList2=[];function tshirtList(){document.getElementById("shopMain2").innerHTML=shopList2.toString()}function addToEndTshirt(){shopList2.push("Tshirt"),tshirtList()}function removeFromEndTshirt(){shopList2.pop(),tshirtList()}function purchase(){let e=Object.keys(shopList).length,n=Object.keys(shopList2).length;alert("Thanks for your order! \n-------------------------- \n"+shopList+"\n"+shopList2+"\n--------------------------\nYour total is €"+(10*e+5*n)+"\nYour item(s) will be delivered in 5 working days!")}function message(){var e=document.getElementById("name").value,n=document.getElementById("email").value;n.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)?alert(e+", thank you for your query. We will contact you via "+n+" shortly."):alert("Please input a valid e-mail address")}$(document).ready(function(){var e={};$("#ageModal").modal("show"),$("#age-submit").on("click",function(){e.month=$("#verify-month").val(),e.day=$("#verify-day").val(),e.year=$("#verify-year").val(),"none"==e.month||"none"==e.day||"none"==e.year?($("#modal-error").css("visibility","visible").hide().fadeIn("slow"),"none"==e.month&&($("#verify-month").css("background","rgba(223,32,44,0.5)"),$("#verify-month").on("change",function(){"none"==$("#verify-month").val()?$("#verify-month").css("background","rgba(223,32,44,0.5)"):$("#verify-month").css("background","white")})),"none"==e.day&&($("#verify-day").css("background","rgba(223,32,44,0.5)"),$("#verify-day").on("change",function(){"none"==$("#verify-day").val()?$("#verify-day").css("background","rgba(223,32,44,0.5)"):$("#verify-day").css("background","white")})),"none"==e.year&&($("#verify-year").css("background","rgba(223,32,44,0.5)"),$("#verify-year").on("change",function(){"none"==$("#verify-year").val()?$("#verify-year").css("background","rgba(223,32,44,0.5)"):$("#verify-year").css("background","white")}))):function n(){var o=moment().subtract(18,"years").calendar();if(moment(e.month+" "+e.day+" "+e.year,"MM DD YYYY").isBefore(o,"day"))$("#ageModal").modal("hide");else{let r;confirm("Sorry, you are not old enough - you will be redirected to another page")&&(window.location="https://www.rte.ie/kids/")}}()})});
+
+
+/* About page Scroller */
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+/* Shop Purchase*/
+
+let shopList=[];
+function jumperList(){
+	document.getElementById("shopMain").innerHTML=shopList.toString();
+}
+function addToEndJumper(){
+	shopList.push("Jumper");
+	jumperList();
+}
+
+function removeFromEndJumper(){
+	+shopList.pop();
+	jumperList();
+}
+
+let shopList2=[];
+function tshirtList(){
+	document.getElementById("shopMain2").innerHTML=shopList2.toString();
+}
+function addToEndTshirt(){
+	shopList2.push("Tshirt");
+	tshirtList();
+}
+function removeFromEndTshirt(){
+	shopList2.pop();
+	tshirtList();
+}
+
+function purchase(){
+	
+	let jumperAmount = Object.keys(shopList).length;
+	let jumperPrice = (jumperAmount * 10);
+	let tshirtAmount = Object.keys(shopList2).length;
+	let tshirtPrice = (tshirtAmount * 5);
+	let totalPrice = jumperPrice + tshirtPrice;
+	
+	alert("Thanks for your order! \n-------------------------- \n"  +shopList +"\n" +shopList2 +"\n--------------------------" +"\nYour total is €" +totalPrice +"\nYour item(s) will be delivered in 5 working days!" );
+}
+
+/* Age Checker */
+
+/*
+Added by James - Start
+*/
+function message(){
+	var name=document.getElementById("name").value;
+	var email=document.getElementById("email").value;
+	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	if(email.match(mailformat)){
+	alert(name+", thank you for your query. We will contact you via "+email+" shortly.");
+	}
+	else
+	{
+	alert("Please input a valid e-mail address");
+	}
+}
+
+
+
+$(document).ready(function () {
+    var age = {};
+      $('#ageModal').modal('show');
+      initAge();
+
+    // starts the age verification process
+    function initAge() {
+      var month = 0;
+      var day = 0;
+      var year = 0;
+
+      $("#age-submit").on("click", function () {
+        age['month'] = $("#verify-month").val();
+        age['day'] = $("#verify-day").val();
+        age['year'] = $("#verify-year").val();
+        checkDate();
+      });
+    }
+    // Check to see if user entered a valid date...
+    function checkDate() {
+      if (age.month == 'none' || age.day == 'none' || age.year == 'none') {
+        // Fade in the error...
+        $('#modal-error').css('visibility', 'visible').hide().fadeIn('slow');
+
+        // changes the background color of the select if invalid
+        if (age.month == 'none') {
+          $("#verify-month").css('background', 'rgba(223,32,44,0.5)');
+          // Look for change of value and change background color when valid
+          $("#verify-month").on('change', function () {
+            if ($("#verify-month").val() == 'none') {
+              $("#verify-month").css('background', 'rgba(223,32,44,0.5)');
+            } else {
+              $("#verify-month").css('background', 'white');
+            }
+          });
+        }
+
+        // changes the background color of the select if invalid
+        if (age.day == 'none') {
+          $("#verify-day").css('background', 'rgba(223,32,44,0.5)');
+          // Look for change of value and change background color when valid
+          $("#verify-day").on('change', function () {
+            if ($("#verify-day").val() == 'none') {
+              $("#verify-day").css('background', 'rgba(223,32,44,0.5)');
+            } else {
+              $("#verify-day").css('background', 'white');
+            }
+          });
+        }
+
+        // changes the background color of the select if invalid
+        if (age.year == 'none') {
+          $("#verify-year").css('background', 'rgba(223,32,44,0.5)');
+          // Look for change of value and change background color when valid
+          $("#verify-year").on('change', function () {
+            if ($("#verify-year").val() == 'none') {
+              $("#verify-year").css('background', 'rgba(223,32,44,0.5)');
+            } else {
+              $("#verify-year").css('background', 'white');
+            }
+          });
+        }
+      } else {
+        oldEnough();
+      }
+    }
+    // Compares age entered with todays date 18 years ago...
+    function oldEnough() {
+      var ageLimit = moment().subtract(18, 'years').calendar();
+      var birthDate = age.month + " " + age.day + " " + age.year;
+      var oldEnough = moment(birthDate, "MM DD YYYY").isBefore(ageLimit, 'day');
+
+      if (oldEnough) {
+        $('#ageModal').modal('hide');
+      } else {
+        /*Augmented script by James */
+		let tooYoung=confirm("Sorry, you are not old enough - you will be redirected to another page");
+		function redirect(){
+		window.location="https://www.rte.ie/kids/";	
+		}
+		if (tooYoung){
+			redirect(tooYoung);
+		}
+	  }
+	}
+	
+});
